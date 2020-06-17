00a563b764de073bb6a8b630b43fe4db32e35aa6
moriarty39 <60015324+moriarty39@users.noreply.github.com>
2020-06-17 01:09:04 -0400

    Update task.js

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,13 +17,16 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
+    
+    document.addEventListener('keydown',
+        (event) => {
+          event instanceof KeyboardEvent
+          if (this.currentSymbol.textContent === event.key) {
+            console.log(this.currentSymbol.textContent + '  ' + event.key)
+            this.success()
+          } else this.fail()
+        }
+      )
   }
 
   success() {
